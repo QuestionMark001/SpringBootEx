52984dde67a48ec4f2354e0b4015100cbdc6af30
QuestionMark001 <604661720@qq.com>
2023-02-22 15:27:11 +0800

    updated Ex1

---
+++ Ex1/src/main/java/com/qm001/ex1/controller/HelloController.java
@@ -0,0 +1,12 @@
+package com.qm001.ex1.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "<p>Hello, SpringBoot<br />你好，SpringBoot</p>";
+    }
+}
