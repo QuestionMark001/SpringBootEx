4b8499720fbf2422922604337943e6e120aaeb5c
QuestionMark001 <604661720@qq.com>
2023-03-12 17:15:28 +0800

    Updated SpringBoot & alibaba-Druid conformity Ex6

---
+++ Ex6/src/main/java/com/qm001/ex6/Ex6Application.java
@@ -0,0 +1,13 @@
+package com.qm001.ex6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex6Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ex6Application.class, args);
+    }
+
+}
---
+++ Ex6/src/test/java/com/qm001/ex6/Ex6ApplicationTests.java
@@ -0,0 +1,24 @@
+package com.qm001.ex6;
+
+import com.qm001.ex6.dao.BookDao;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex6ApplicationTests {
+
+    @Autowired
+    private BookDao bookDao;
+
+    @Test
+    void selectTest_0() {
+        System.out.println(bookDao.selectById(1));
+    }
+
+    @Test
+    void selectTest_1() {
+        System.out.println(bookDao.selectById(3));
+    }
+
+}
