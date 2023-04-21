66f2a3aa43a58bd4cfa492f57a4a4a620cadfbf1
QuestionMark001 <604661720@qq.com>
2023-04-21 08:41:29 +0800

    Updated SpringBootEx Final Project

--- Ex7/src/main/java/com/qm001/ex7/Ex7Application.java
+++ Ex7/src/main/java/com/qm001/ex7/Ex7Application.java
@@ -3,6 +3,7 @@ package com.qm001.ex7;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 @Slf4j
 @SpringBootApplication
 public class Ex7Application {
